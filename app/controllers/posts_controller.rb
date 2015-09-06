0f78a896ea421e06001e77e68de89d9e56dd9b49
Sumeet <ursumeetm@gmail.com>
2015-09-06 18:20:42 +0530

    Updated Homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,9 @@ class PostsController < ApplicationController
 		@quotes = Quote.all.order("created_at DESC")
 		@images = Image.all.order("created_at DESC")
 		@jokes = Joke.all.order("created_at DESC")
+		@facts = Fact.all.order("created_at DESC")
+
+		@items = (@quotes.to_a + @jokes.to_a + @images.to_a + @facts.to_a).reverse
+
 	end
 end
