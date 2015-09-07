73610330f3451191f0f70e560454ba135748cdc4
Sumeet <ursumeetm@gmail.com>
2015-09-07 18:20:18 +0530

    Fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default_user_avatar.png",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-
+  end
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
